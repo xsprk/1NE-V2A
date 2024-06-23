b19c5a9f7644505ea424f1e5315e8a1033ac9b09
king101125s@163.com <king101125s@gmail.com>
2024-06-23 23:31:25 +0800

    fix GoogleAnalytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -4,14 +4,17 @@ export const GoogleAnalyticsTracking = () => {
     return (
         <>
             {/* <!-- Google tag (gtag.js) --> */}
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-M0S8FPNR8E"></script>
-            <script>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M0S8FPNR8E"></Script>
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+                function gtag(){window.dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', 'G-M0S8FPNR8E');
-            </script>
+                `}
+            </Script>
+
         </>
     );
 }

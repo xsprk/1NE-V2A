cff78a3fbec430d7bc71fa285f3754f407ca58f7
king101125s@163.com <king101125s@gmail.com>
2024-06-23 09:25:21 +0800

    add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-K5WPSNPR" />
       <body className={inter.className}>{children}</body>
     </html>
   );

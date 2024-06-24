3b2c9530eca524f58558e85922c88e36fd26b322
king101125s@163.com <king101125s@gmail.com>
2024-06-24 14:33:48 +0800

    add file name size

--- components/video-uploader.tsx
+++ components/video-uploader.tsx
@@ -46,38 +46,40 @@ export default function VideoUploader({
   );
 
   return (
-    <>
-      <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-          </svg>
-          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className='font-semibold'>Click to upload</span> or drag and drop</p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">wav,mp3,flac,mp4,mov,mkv,avi,mpeg,webm</p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">Max file size: 50MB</p>
-        </div>
-        <div className="mt-1 flex rounded-md shadow-sm">
-          <input
-            id="dropzone-file"
-            name="dropzone-file"
-            type="file"
-            accept="audio/*,video/*"
-            className="sr-only"
-            onChange={onChangeAudio}
-            {...props}
-          />
-        </div>
-        {data.audio && (
+    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-66 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+        </svg>
+        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className='font-semibold'>Click to upload</span> or drag and drop</p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">wav,mp3,flac,mp4,mov,mkv,avi,mpeg,webm</p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">Max file size: 50MB</p>
+      </div>
+      <div className="mt-1 flex rounded-md shadow-sm">
+        <input
+          id="dropzone-file"
+          name="dropzone-file"
+          type="file"
+          accept="audio/*,video/*"
+          className="sr-only"
+          onChange={onChangeAudio}
+          {...props}
+        />
+      </div>
+      {data.audio && file && (
+        <>
+          <p className="text-sm text-gray-500 dark:text-gray-400">{file.name}</p>
+          <p className="text-sm text-gray-500 dark:text-gray-400">{file.size / 1024 / 1024} MB</p>
           <audio controls src={data.audio} className="w-full h-16 mt-4">
             <track kind="captions" src="/srt/test.vtt" label="English" />
           </audio>
-          // <video className="w-full h-auto max-w-full border border-gray-200 rounded-lg dark:border-gray-700" muted controls>
-          //   <source src={data.audio} type="video/mp4" />
-          //   Your browser does not support the video tag.
-          //   <track kind="captions" src="/srt/test.vtt" label="English" />
-          // </video>
-        )}
-      </label>
-    </>
+        </>
+        // <video className="w-full h-auto max-w-full border border-gray-200 rounded-lg dark:border-gray-700" muted controls>
+        //   <source src={data.audio} type="video/mp4" />
+        //   Your browser does not support the video tag.
+        //   <track kind="captions" src="/srt/test.vtt" label="English" />
+        // </video>
+      )}
+    </label>
   );
 }

cdd0f971baa2bbe5c72bde0542a82e6026553a92
Xspark <151998806+xsprk@users.noreply.github.com>
2025-09-21 14:59:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >

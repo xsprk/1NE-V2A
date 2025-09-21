d2cb862b698667f29330e898e6cd4270e38a161e
Xspark <151998806+xsprk@users.noreply.github.com>
2025-09-21 16:35:57 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export function Footer() {
     return (
         <footer className="fixed bottom-0 left-0 z-20 w-full p-4 border-t border-gray-200 shadow bg-white dark:bg-gray-800 dark:border-gray-600 flex justify-center">
-  <span className="text-sm text-gray-500 dark:text-gray-400">
+  <span className="text-sm text-gray-500">
     ©2025 <a href="https://v2a.1ne.in.net" className="hover:underline">1NE Networks Ltd</a>. All Rights Reserved.
   </span>
 </footer>

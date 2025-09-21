54efc1b831c63d1427b318d444a169bd293033c8
Xspark <151998806+xsprk@users.noreply.github.com>
2025-09-21 14:49:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
     default: 'video to audio converter',
   },
   description: "A Browser-Based Video to Audio Converter",
-  metadataBase: new URL(''),
+  metadataBase: new URL('v2a.1ne.in.net'),
 };
 
 export default function RootLayout({

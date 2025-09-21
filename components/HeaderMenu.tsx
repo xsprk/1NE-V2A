a063ebacdfaafa8af8d5be1967ba3629c7e7b3d5
Xspark <151998806+xsprk@users.noreply.github.com>
2025-09-21 15:15:41 +0530

    Update HeaderMenu.tsx

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -6,8 +6,10 @@ import Link from "next/link"
 import Image from "next/image";
 export function HeaderMenu() {
     return (
-        <nav className=" border-gray-200 dark:border-gray-600 dark:bg-gray-900 border-b">
-             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">1NE - Video2Audio</span>
-        </nav>
+        <nav className="border-gray-200 dark:border-gray-600 dark:bg-gray-900 border-b flex justify-center">
+  <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">
+    1NE - Video2Audio
+  </span>
+</nav>
     )
 }

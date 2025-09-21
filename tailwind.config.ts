508ec32128a3b9ee8f50b41825757efb5d41b566
Xspark <151998806+xsprk@users.noreply.github.com>
2025-09-21 16:37:53 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 
 const config = {
-  darkMode: ["false"],
+  darkMode: false,
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',

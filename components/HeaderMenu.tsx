81509e65bf360a501f6061a36429e0b96d8a3e3e
Xspark <151998806+xsprk@users.noreply.github.com>
2025-09-21 14:54:03 +0530

    Update HeaderMenu.tsx

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -4,14 +4,10 @@
 
 import Link from "next/link"
 import Image from "next/image";
-import { ModeToggle } from "./darkmodel/modeToggle";
-
 export function HeaderMenu() {
     return (
         <nav className=" border-gray-200 dark:border-gray-600 dark:bg-gray-900 border-b">
-             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">video2audio</span>
-
-                    <ModeToggle />
+             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Video2Audio</span>
         </nav>
     )
 }

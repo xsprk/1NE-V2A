b2d391cef64932d05fe3f0b61571ddef37736898
Xspark <151998806+xsprk@users.noreply.github.com>
2025-09-21 14:19:55 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,15 +5,9 @@ import Link from "next/link"
 export function Footer() {
     return (
         <footer className="fixed  bottom-0 left-0 z-20 w-full p-4 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600   ">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://qianniuspace.com/" className="hover:underline">qianniu</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="v2a.1ne.in.net" className="hover:underline">1NE Networks Ltd</a>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                <li>
-                    <Link href="https://qianniuspace.com" className="hover:underline me-4 md:me-6">About</Link>
-                </li>
-                <li>
-                    <Link href="https://github.com/qianniucity/video2audio" className="hover:underline">Github</Link>
-                </li>
             </ul>
         </footer>
     )
